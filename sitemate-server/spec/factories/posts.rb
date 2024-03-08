b1f02577be1d6017e47e03eb1e55ae5307991556
Daniel Maddern <maddernd@deakin.edu.au>
2024-03-08 15:09:54 +1100

    add basic unit test for post controller

---
+++ sitemate-server/spec/factories/posts.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+    factory :post do
+      title { "Sample Post Title" }
+      content { "Sample post content." }
+
+    end
+  end
+  
--- sitemate-server/spec/posts_controller_spec.rb
+++ sitemate-server/spec/posts_controller_spec.rb
@@ -4,6 +4,11 @@ RSpec.describe PostsController, type: :controller do
   let!(:posts) { create_list(:post, 3) }
   let(:post_id) { posts.first.id }
 
+  # Helper method to parse JSON response
+  def json
+    JSON.parse(response.body)
+  end
+
   # Test suite for GET /posts
   describe 'GET #index' do
     before { get :index }
@@ -11,3 +16,6 @@ RSpec.describe PostsController, type: :controller do
     it 'returns all posts' do
       expect(json).not_to be_empty
       expect(json.size).to eq(3)
+    end
+  end
+end
